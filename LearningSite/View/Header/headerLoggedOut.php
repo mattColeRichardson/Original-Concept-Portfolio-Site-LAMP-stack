5218c8cc8b673dfa01cf0ae922cae4033e49adcf
Matthew Richardson <mattcolerichardson@gmail.com>
2020-03-27 14:07:24 -0500

    login screen looking good

--- LearningSite/View/Header/headerLoggedOut.php
+++ LearningSite/View/Header/headerLoggedOut.php
@@ -2,4 +2,3 @@
 <?php
 include 'View/Header/html/loginForm.html';
 ?>
-<button class="button" name = "newuser" onclick="location.href='View/Header/html/createNewUserForm.html'" type="button">Create New User</button>
